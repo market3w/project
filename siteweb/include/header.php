6f69b0e0f5ff14987b4c5b2f0909dc5328c988f7
vincerasta <vincentabensour@msn.com>
2014-08-03 20:04:11 +0200

    Mise à jour design :
    Refonte du menu + connexion en responsive
    Ajout du contenu dans la page "société"
    Refonte des pages articles et vidéos

---
+++ siteweb/include/header.php
@@ -0,0 +1,97 @@
+ 
+        <!-- Header -->
+        
+        	 <div class="logo_tablette" ><div style="background-color:#83BBD3;  border-radius:3px; width:40px; height:40px; padding:5px;margin-top:-10px;cursor:pointer; float:right;" class="btn_connexion"><img src="images/em.png" style="width:30px;" /></div><center><a href="index.php" style="text-decoration:none;"><img src="images/logo_mini.png" width="120" /></a></center></div>
+             <div class="logo_mobile" ><div style="background-color:#83BBD3;  border-radius:3px; margin-right:3px;width:30px; height:30px; padding:5px;margin-top:3px;cursor:pointer; float:right;" class="btn_connexion"><img src="images/em.png" style="width:20px;" /></div><a href="index.php" style="text-decoration:none;"><img src="images/logo_mini.png" width="100" style="margin-left:10px;  margin-bottom:5px; margin-top:8px;"/></a></div>
+            
+			<header id="header">
+            
+				<h1 id="logo"><a href="index.php"><img src="images/logo_mini.png" style="width:110px;"/></a></h1>
+				<nav id="nav">
+					<ul>
+						
+						<li class="submenu">
+							<a href="" style="color:#ffffff; font-weight:700;">Menu</a>
+							<ul>
+								<li><a href="index.php">Accueil</a></li>
+                            <li><a href="societe.php">La société</a></li>
+								<li><a href="services.php">Nos services</a></li>
+								
+								<li class="submenu">
+									<a href="tutoriels.php">Les tutoriels</a>
+									<ul>
+										<li><a href="articles.php">Documents pdf</a></li>
+										<li><a href="videos.php">Les vidéos</a></li>
+									</ul>
+								</li>
+                                <li><a href="contact.php">Contact</a></li>
+							</ul>
+						</li>
+						<li><a href="#" class="button special btn_connexion" >Connexion</a></li>
+					</ul>
+				</nav>
+			</header>
+            
+             <script type="text/javascript">
+	
+  		  $(document).ready(function() {
+	
+	 	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Confirmation des parametres !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	   $(".btn_connexion").on('click', function(){
+				$(".fermer_pop_up").fadeIn(500);
+				$("#conteneur_connexion").fadeIn(500);
+				
+			});
+			
+			 $(".fermer_pop_up").on('click', function(){
+				$(".fermer_pop_up").fadeOut(500);
+				$("#conteneur_connexion").fadeOut(500);
+				
+			});
+			
+			 $("#oubli_mdp").on('click', function(){
+				$("#formulaire_connexion").hide(500);
+				$("#conteneur_oubli_mdp").show(500);
+				$("#titre_conteneur_connexion").html("MOT DE PASSE OUBLIE");
+			});
+			
+			$("#annul_oubli_mdp").on('click', function(){
+				$("#conteneur_oubli_mdp").hide(500);
+				$("#formulaire_connexion").show(500);
+				$("#titre_conteneur_connexion").html("CONNEXION");
+			});
+           
+        });
+		</script>
+            </head>
+			<body class="index loading">
+            <div class="fermer_pop_up" style="position:fixed; cursor:pointer; z-index:13000; display:none; background-color: rgba(0, 0, 0, 0.70); width:100%; height:100%; "></div>
+  			
+             <div id="conteneur_connexion">
+   
+  <center> <h1 style="color:#ffffff; background-color:#83BBD3; padding:10px;"><span id="titre_conteneur_connexion">Connexion</span><span class="fermer_pop_up" style="float:right; cursor:pointer;">X</span></h1></center>
+   <div id="formulaire_connexion" style="padding:10px;"><center>
+   <form action="" method="post">
+   <input type="text" name="login" value="" style="width:80%;" placeholder="Pseudo"/><br/>
+   <input type="password" name="password" style="width:80%;" value=""  placeholder="Mot de passe" /><br/>
+   <input type="submit" name="submit" value="SE CONNECTER" style="background-color:#83BBD3; border:0px; color:#ffffff; cursor:pointer;" /><br/><br/>
+   </form>
+   </center>
+    <div style="float:left; margin-bottom:8px;"><a href="#" style=" color:#4F394F;" id="oubli_mdp">Mot de passe oublié ?</a></div> <div style="float:right; margin-bottom:8px;"><a href="inscription.php" style=" color:#4F394F;">Inscription</a></div>
+
+   </div>
+   
+   <div id="conteneur_oubli_mdp" style="padding:10px; display:none;"><center>
+   <form action="" method="post">
+   <input type="text" name="email" value="" style="width:80%;" placeholder="Email"/><br/>
+   
+   <input type="submit" name="submit" value="ENVOYER" style="background-color:#83BBD3; border:0px; color:#ffffff; cursor:pointer;" /><br/>
+    <a href="#" style=" color:#4F394F;" id="annul_oubli_mdp">Annuler</a>
+   </form>
+   </center>
+   
+
+   </div>
+   
+     </div>
+           
---
+++ siteweb/index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE HTML>
+<!--
+	Twenty 1.0 by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>MARKET 3W | Agende de référencement</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<?php 
+			include('include/head.php');
+		?>
+	</head>
+	<body class="index loading">
+	
+		<?php
+			include('include/header.php');
+		?>
+
+		<!-- Banner -->		
+			<section id="banner">
+				
+				<!--
+					".inner" is set up as an inline-block so it automatically expands
+					in both directions to fit whatever's inside it. This means it won't
+					automatically wrap lines, so be sure to use line breaks where
+					appropriate (<br />).
+				-->
+				<div class="inner">
+					
+					<header>
+						<h2><img src="images/logo.png" style="margin-top:10px; width:100%;"/></h2>
+					</header>
+					<p>Agence de <strong>Webmarketing</strong><br />
+                    spécialisée en référencement
+										<footer>
+						<ul class="buttons vertical">
+							<li><a href="#main" class="button fit scrolly">Voir la suite...</a></li>
+						</ul>
+					</footer>
+				
+				</div>
+				
+			</section>
+		
+		<!-- Main -->
+			<article id="main">
+
+				<header class="special container">
+					<span class="icon fa-bar-chart-o"></span>
+					<h2>Market <strong>3W</strong> garantit des résultats rapides et efficaces en matière de référencement.
+					</h2>
+					<p>Profitez de notre expertise en marketing digital afin de promouvoir efficacement votre site E-Commerce.<br/><br/>
+
+ 
+
+Composé d'une équipe de Webmarketeurs expérimentés soigneusement sélectionnés pour leurs compétences inéluctables, Market3W vous accompagne dans votre projet vers un gain de trafic qualifié.<br/><br/>
+
+Market3W étant spécialisé dans le référencement, nous appliquons toutes les différentes techniques de référencement qui puissent exister. A savoir, le référencement naturel (SEO, gratuit), le référencement payant (SEA, achat de mots-clés), le référencement grâce aux réseaux sociaux (SMO, alimentation des pages de réseaux sociaux)...  <br/><br/>
+
+
+
+  </p>
+                    	<br/><a href="services.php" class="button fit scrolly" style="width:400px;">Voir tous nos services</a>
+				</header>
+					
+					
+						
+						
+				
+				<!-- One -->
+					<section class="wrapper style2 container special-alt" id="video_conference">
+						<div class="row half">
+							<div class="12u">
+							<header>
+									<h2>Contacter un webmarketeur par <strong>Vidéo-conférence</strong></h2>
+								</header>
+								<p><center>Vous pouvez prendre gratuitement un rendez-vous avec un de nos web-marketeur qui vous aiguillera dans vos décisions. Nous avons mis en place un système de vidéo-conférence sur le site. Il suffit de vous inscrire ou de vous connecter.</center></p>
+								<footer>
+									<center><ul class="buttons">
+										<li><a href="#" class="button">Fixer un rendez-vous</a></li>
+									</ul></center>
+								</footer>
+							
+							</div>
+								
+						</div>
+					</section>
+					
+			
+					
+				<!-- Three -->
+					<section class="wrapper style3 container special">
+					
+						<header class="major">
+							<h2>Nos dernières <strong>prestations</strong></h2>
+						</header>
+						
+						<div class="row">
+							<div class="3u">
+								<section>
+									<a href="#" class="image feature"><img src="images/pic01.jpg" alt="" /></a>
+								</section>
+							</div>
+                            <div class="3u">
+								<section>
+									<a href="#" class="image feature"><img src="images/pic02.jpg" alt="" /></a>
+								</section>
+							</div>
+                            <div class="3u">
+								<section>
+									<a href="#" class="image feature"><img src="images/pic03.jpg" alt="" /></a>
+								</section>
+							</div>
+                             <div class="3u">
+								<section>
+									<a href="#" class="image feature"><img src="images/pic04.jpg" alt="" /></a>
+								</section>
+							</div>
+							
+						</div>
+                        
+						
+						<footer class="major">
+							<ul class="buttons">
+								<li><a href="#" class="button">En Savoir +</a></li>
+							</ul>
+						</footer>
+					
+					</section>
+					
+			</article>
+
+		<!-- CTA -->
+			<section id="cta">
+			
+				<header>
+					<h2>Besoin d'aide pour <strong>le référencement </strong>?</h2>
+					<p>Télécharger nos tutoriels en PDF gratuitement ou regardez des vidéos explicatives.</p>
+				</header>
+				<footer>
+					<ul class="buttons">
+						<li><a href="#" class="button">Tutoriels pdf</a></li>
+						<li><a href="#" class="button">Voir les vidéos</a></li>
+					</ul>
+				</footer>
+			
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+			
+				<ul class="icons">
+					<li><a href="#" class="icon circle fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="#" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>
+					<li><a href="#" class="icon circle fa-google-plus"><span class="label">Google+</span></a></li>
+					<li><a href="#" class="icon circle fa-github"><span class="label">Github</span></a></li>
+					<li><a href="#" class="icon circle fa-dribbble"><span class="label">Dribbble</span></a></li>
+				</ul>
+				
+				<span class="copyright">&copy; MARKET 3W. Tout droits réservés.</span>
+			
+			</footer>
+
+	</body>
+</html>
