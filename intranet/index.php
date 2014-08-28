e84cb9995c1051a7ddba863104e1d86c04a4e289
fthierry <thierryflorian@free.fr>
2014-08-28 11:18:02 +0200

    Intégration du calendrier + modif mail

--- intranet/index.php
+++ intranet/index.php
@@ -55,89 +55,67 @@ else
 <script src="js/chart.min.js" type="text/javascript"></script> 
 <script src="js/bootstrap.js"></script>
 <script language="javascript" type="text/javascript" src="js/full-calendar/fullcalendar.js"></script>
+<script language="javascript" type="text/javascript" src="js/full-calendar/actions.js"></script>
  
 <script src="js/base.js"></script> 
-<script>     
-
-      
-
-        $(document).ready(function() {
-        var date = new Date();
-        var d = date.getDate();
-        var m = date.getMonth();
-        var y = date.getFullYear();
-        var calendar = $('#calendar').fullCalendar({
-          header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay'
-          },
-          selectable: true,
-          selectHelper: true,
-          select: function(start, end, allDay) {
-            var title = prompt('Event Title:');
-            if (title) {
-              calendar.fullCalendar('renderEvent',
-                {
-                  title: title,
-                  start: start,
-                  end: end,
-                  allDay: allDay
-                },
-                true // make the event "stick"
-              );
-            }
-            calendar.fullCalendar('unselect');
-          },
-          editable: true,
-          events: [
-            {
-              title: 'All Day Event',
-              start: new Date(y, m, 1)
-            },
-            {
-              title: 'Long Event',
-              start: new Date(y, m, d+5),
-              end: new Date(y, m, d+7)
-            },
-            {
-              id: 999,
-              title: 'Repeating Event',
-              start: new Date(y, m, d-3, 16, 0),
-              allDay: false
-            },
-            {
-              id: 999,
-              title: 'Repeating Event',
-              start: new Date(y, m, d+4, 16, 0),
-              allDay: false
-            },
-            {
-              title: 'Meeting',
-              start: new Date(y, m, d, 10, 30),
-              allDay: false
-            },
-            {
-              title: 'Lunch',
-              start: new Date(y, m, d, 12, 0),
-              end: new Date(y, m, d, 14, 0),
-              allDay: false
-            },
-            {
-              title: 'Birthday Party',
-              start: new Date(y, m, d+1, 19, 0),
-              end: new Date(y, m, d+1, 22, 30),
-              allDay: false
-            },
-            {
-              title: 'EGrappler.com',
-              start: new Date(y, m, 28),
-              end: new Date(y, m, 29),
-              url: 'http://EGrappler.com/'
-            }
-          ]
-        });
-      });
-    </script><!-- /Calendar -->
+<script> 
+$(document).ready(function() {
+	var date = new Date();
+	var d = date.getDate();
+	var m = date.getMonth();
+	var y = date.getFullYear();
+	var calendar = $('#calendar').fullCalendar({
+		header: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'month',
+		},
+		minTime: '09:00:00',
+		maxTime: '19:00:00',
+		allDaySlot: false,
+		weekends: false,
+		selectable: true,
+		selectHelper: true,
+		select: function(start, end, allDay) {
+			if ($('#calendar').fullCalendar('getView').name=="month") {
+				// Clicked on the entire day
+				$('#calendar')
+				.fullCalendar('changeView', 'agendaDay')
+				.fullCalendar('gotoDate',start);
+				$("#calendar .fc-content").css("padding-top","20px");
+			} else {
+				addAppointment(start,end);
+			}
+		},
+		editable: false,
+		// A modifier pour récupérer les infos dynamiquement
+		events: [
+			{
+				id: 5,
+				title: 'Lunch',
+				start: new Date(y, m, d, 12, 0),
+				end: new Date(y, m, d, 14, 0),
+				allDay: false
+			},
+			{
+				id: 9,
+				type: 'unavailable',
+				title: 'Indisponnible',
+				start: new Date(y, m, d, 16, 0),
+				end: new Date(y, m, d, 17, 0),
+				allDay: false
+			},
+			{
+				id: 10,
+				title: 'Vidéoconférence',
+				start: new Date(y, m, 25, 12, 0),
+				end: new Date(y, m, 25, 14, 0),
+				allDay: false
+			}
+		]
+	});
+	
+});
+</script><!-- /Calendar -->
 </body>
 </html>
