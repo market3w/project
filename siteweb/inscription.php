211e74bd516d5ad72d81d797fe632321a4ead61c
vincerasta <vincentabensour@msn.com>
2014-08-31 01:02:23 +0200

    Mise à jour inscription prospect fonctionnel coté serveur mais ne fonctionne pas côté clien + rajouter société

--- siteweb/inscription.php
+++ siteweb/inscription.php
@@ -27,7 +27,7 @@
 
 				<div style="text-align:center; padding-left:20px; padding-right:20px;">
 					<h2>Inscription</h2>
-					<p>Vous désirez télécharger un de nos tutoriels, devenir client, utiliser la vidéo conférence, booster votre référencement, inscrivez-vous, cela prend deux minutes.</p>
+					<p>Vous désirez devenir client et ainsi booster votre référencement, inscrivez-vous, cela prend deux minutes.</p>
 				</div>
 					
 				<!-- One -->
@@ -35,47 +35,88 @@
 					
 						<!-- Content -->
 							<div class="content">
-								<form>
+                           <span class="responseError" id="loginError"><?php echo $_SESSION["errorMessage"]; ?></span>
+                   
+								<form action="inscription.php" id="post_userForm" method="post">
+                                
+                                <input type="hidden" name="methode" value="post_user" />
 									<div class="row half no-collapse-1">
 										<div class="6u">
-											<input type="text" name="name" placeholder="Nom" />
+											<input type="text" name="user_name" placeholder="Nom" />
 										</div>
 										<div class="6u">
-											<input type="text" name="prenom" placeholder="Prenom" />
+											<input type="text" name="user_firstname" placeholder="Prenom" />
+										</div>
+									</div>
+                                    <div class="row half">
+										<div class="12u">
+											<input type="text" name="user_email" placeholder="Email" />
+										</div>
+									</div>
+									<div class="row half">
+										<div class="6u">
+											<input type="text" name="user_password" placeholder="Mot de passe" />
+										</div>
+                                        <div class="6u">
+											<input type="password" name="user_password2" placeholder="Confirmer ot de passe" />
+										</div>
+									</div>
+                                    <br/>
+                                    <div class="row half">
+										<div class="12u">
+											<input type="text" name="user_adress" placeholder="Adresse" />
 										</div>
 									</div>
 									<div class="row half">
 										<div class="12u">
-											<input type="text" name="adresse" placeholder="Adresse" />
-										</div>
-									</div>
-									<div class="row half">
-										<div class="12u">
-											<input type="text" name="adresse 2" placeholder="Adresse complémentaire" />
+											<input type="text" name="user_adress2" placeholder="Adresse complémentaire" />
 										</div>
 									</div>
                                     <div class="row half no-collapse-1">
 										<div class="6u">
-											<input type="text" name="cp" placeholder="Code postal" />
+											<input type="text" name="user_zipcode" placeholder="Code postal" />
 										</div>
 										<div class="6u">
-											<input type="text" name="ville" placeholder="Ville" />
+											<input type="text" name="user_town" placeholder="Ville" />
 										</div>
 									</div>
-									<div class="row">
+									
+                                    <div class="row half">
+										<div class="6u">
+											<input type="text" name="user_phone" placeholder="Téléphone" />
+										</div>
+                                        <div class="6u">
+											<input type="text" name="user_mobile" placeholder="Portable" />
+										</div>
+									</div>
+                                    <div class="row half">
+										<div class="6u">
+											<input type="text" name="user_function" placeholder="Fonction" />
+										</div>
+                                    </div>
+                                    <div class="row">
 										<div class="12u">
 											<ul class="buttons">
-												<li><a href="#" class="button">Inscription</a></li>
+												<a href="#" class="button inscriptionbtn">Inscription</a>
 											</ul>
 										</div>
 									</div>
+                                    
 								</form>
 							</div>
 							
 					</section>
 					
 			</article>
+		<script type="text/javascript">
+	$(document).ready(function() {
 
+	$(".inscriptionbtn").on('click', function(){
+				$("#post_userForm").submit();
+				return false;
+			});
+		});
+		</script>
 		<?php
 			include('include/footer.php');
 		?>
