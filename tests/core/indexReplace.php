5d0b5fb023e2aec8375ec36bee0cdebfd246b158
fthierry <thierryflorian@free.fr>
2014-09-01 16:08:54 +0200

    test

--- tests/core/indexReplace.php
+++ tests/core/indexReplace.php
@@ -12,10 +12,23 @@ switch(FOLDER){
 }
 
 function apiIndexReplace(){
-    require_once BASE_SRC."api/Application/Configs/Settings.php";
     require_once BASE_SRC."api/Library/Core/Connexion.php";
     
-    $iSettings = new Application_Configs_Settings();
+    define("RTC_ROOT", "http://videoconference.market3w.test.com");
+    define("INTRANET_ROOT", "http://intranet.market3w.test.com");
+    define("VITRINE_ROOT", "http://www.market3w.test.com");
+        
+    define("DB_HOST",   "localhost");
+    define("DB_NAME",   "market3w");
+    define("DB_USER",   "market3w_user");
+    define("DB_PASS",   "Gru15;7T$");
+    define("DB_CHARSET","utf8");
+
+    define("SALT_USER_PWD", '=+/e-Kue/vW--y?cj:,54rsfgh:lm&d5eE>tl#rmzh./M+Jz47a,sz>BARyXrRXZ4P~%');
+
+    date_default_timezone_set("Europe/Paris");
+    ini_set('session.gc_maxlifetime', 7200);
+    ini_set('session.cookie_lifetime', 7200);
 
     $connexion = new Library_Core_Connexion();
     $connexion->connect(DB_HOST, DB_NAME, DB_USER, DB_PASS, DB_CHARSET);
