0c3b9abf408fc241caea4925774fa8fd9db36578
fthierry <thierryflorian@free.fr>
2014-09-01 19:31:07 +0200

    Maj pour jenkins

---
+++ tests/api/appointmentsTest.php
@@ -0,0 +1,43 @@
+<?php
+define("BASE_TEST", (file_exists("C:/wamp/www/market3w-test/tests/core/indexReplace.php"))?"C:/wamp/www/market3w-test/tests/":"/var/lib/jenkins/jobs/market3w-test/workspace/tests/");
+define("BASE_SRC", str_replace("tests", "src", BASE_TEST));
+define("FOLDER","api");
+
+require_once BASE_TEST."core/indexReplace.php";
+
+class AppointmentsTest extends TestMaster {
+
+    /**
+     * @var Appointments
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    public function setUp() {
+        $this->object = new Application_Controllers_Appointments;
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown() {
+        
+    }
+
+    /**
+     * @covers Application_Controllers_Appointments::get_allappointment
+     * @todo   Implement testGet_allappointment().
+     */
+    public function testGet_allappointment() {
+        $res = $this->object->get_allappointment(array());
+        $this->assertFalse($res->apiError);
+        $this->assertEquals('',$res->apiErrorMessage);
+        $this->assertFalse($res->serverError);
+        $this->assertEquals('',$res->apiErrorMessage);
+    }
+
+}
---
+++ tests/core/testMaster.php
@@ -0,0 +1,18 @@
+<?php
+class TestMaster extends PHPUnit_Framework_TestCase {
+    public function __construct() {        
+        spl_autoload_register(array($this,'class_autoload'));
+    }
+    
+    protected function setUp() {
+    }
+    
+    protected function tearDown() {
+    }
+    
+    public function class_autoload($class){
+        if(strpos($class, "PHPUnit")===false){
+            require_once BASE_SRC.FOLDER."/".str_replace("_", "/", $class).".php";
+        }
+    }
+}
